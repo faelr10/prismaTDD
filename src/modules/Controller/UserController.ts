681aa5b1282a2bb277f4ca662dc10768cc0de161
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-01-30 01:43:11 -0300

    alter structure folder User

--- src/modules/Controller/UserController.ts
+++ src/modules/Controller/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CreateUserService } from "./CreateUserService";
+import { CreateUserService } from "../Service/UserService";
 
 class CreateUserController {
 
