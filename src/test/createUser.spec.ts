f429d701572560750562baf416fcf263364880fb
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-01-31 12:45:55 -0300

    add test endpoint getUsers

--- src/test/createUser.spec.ts
+++ src/test/createUser.spec.ts
@@ -1,20 +1,36 @@
 import { app } from "../app";
 import request from "supertest";
 import { UserRepository } from "../modules/Repository/UserRepository";
+import { prismaClient } from "../database/prismaClient";
 
 describe("test endpoints Users", () => {
   it("testando create User", async () => {
 
     const name = "Rafael"
-
+    
     const response = await request(app).post("/create").send({name:name})
     console.log(response)
     expect(response.status).toBe(201)
     expect(response.body).toHaveProperty("id")
 
     await new UserRepository().delete(response.body.id)
-    
+
   });
 
+  it("test get User", async () => {
+
+    const user1 = await prismaClient.user.create({ data: { name: "Rafael" } })
+    const user2 = await prismaClient.user.create({ data: { name: "Adriano" } })
+    const user3 = await prismaClient.user.create({ data: { name: "Waguininho" } })
+
+    const response = await request(app).get("/findAll")
+    console.log(response)
+    expect(response.body).toHaveLength(3)
+
+    await prismaClient.user.delete({ where: { id: user1.id } })
+    await prismaClient.user.delete({ where: { id: user2.id } })
+    await prismaClient.user.delete({ where: { id: user3.id } })
+
+  })
 
 });
